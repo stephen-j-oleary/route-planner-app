39ac57e1aa02d2599421fc778e5cb9b85b62b08a
stephen-j-oleary <s.james.oleary@gmail.com>
2022-10-31 20:39:53 -0600

    Transition to use nextjs

---
+++ pages/_app.js
@@ -0,0 +1,24 @@
+
+import "../shared/styles/globals.css";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { store } from "../redux/store.js";
+import { Provider } from "react-redux";
+import Head from "next/head.js";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <React.StrictMode>
+      <Provider store={store}>
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="theme-color" content="#000000" />
+          <meta name="description" content="Route Planner Website" />
+          <title>React App</title>
+        </Head>
+        <Component {...pageProps} />
+      </Provider>
+    </React.StrictMode>
+  );
+}
---
+++ pages/_document.js
@@ -0,0 +1,16 @@
+
+import { Html, Head, Main, NextScript } from "next/document.js"
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/logo192.png" />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
+      <Main />
+      <NextScript />
+    </Html>
+  )
+}
