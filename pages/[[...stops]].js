baf67218573a3139181d41b738296ee1d1688e17
stephen-j-oleary <s.james.oleary@gmail.com>
2022-11-15 23:05:01 -0700

    Add list view option for stop input

--- pages/[[...stops]].js
+++ pages/[[...stops]].js
@@ -1,12 +1,22 @@
 
 import styles from "../shared/styles/stops.module.scss";
+import classNames from "classnames";
 import Header from "../components/Header";
 import Map from "../components/Map";
 import DirectionsForm from "../components/DirectionsForm";
+import { useSelector } from "react-redux";
+import { selectViewMode } from "../redux/slices/routeForm.js";
 
 export default function App() {
+  const viewMode = useSelector(selectViewMode);
+
   return (
-    <div className={styles.app}>
+    <div
+      className={classNames(
+        styles.app,
+        { [styles.listMode]: viewMode === "list" }
+      )}
+    >
       <Header className={styles.header} />
       <aside className={styles.sidebar}>
         <DirectionsForm />
