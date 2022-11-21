45a0047c41266fac24191cc8522cc2319d2fd607
stephen-j-oleary <s.james.oleary@gmail.com>
2022-11-20 18:59:22 -0700

    Prevent extra rerenders

--- shared/hooks/useStops.js
+++ shared/hooks/useStops.js
@@ -2,6 +2,7 @@
 import _ from "lodash";
 import useURL from "./useURL.js";
 import { fromStopString, toStopString } from "../Stop.js";
+import { useCallback } from "react";
 
 export default function useStops() {
   const [url, setUrl] = useURL();
@@ -14,23 +15,25 @@ export default function useStops() {
     .map(fromStopString)
     .value();
 
-  const setStops = (value, index = null) => {
-    const newStops = _.chain(stops)
-      .cloneDeep()
-      .thru(val => index
-        ? _.set(val, index, value)
-        : value)
-      .map(toStopString)
-      .map(encodeURIComponent)
-      .join("/")
-      .thru(val => "/" + val)
-      .value();
+  const setStops = useCallback(
+    (value) => {
+      if (!url) return;
 
-    const urlCpy = new URL(url);
-    urlCpy.pathname = newStops;
+      const newStops = _.chain(value)
+        .cloneDeep()
+        .map(toStopString)
+        .map(encodeURIComponent)
+        .join("/")
+        .thru(val => "/" + val) // Add leading slash
+        .value();
 
-    setUrl(urlCpy, "replace", { shallow: true });
-  };
+      const urlCpy = new URL(url);
+      urlCpy.pathname = newStops;
+
+      setUrl(urlCpy, "replace", { shallow: true });
+    },
+    [url, setUrl]
+  );
 
   return [
     stops,
