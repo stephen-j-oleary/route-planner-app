a3bb0c56350c70398b6c10ec904f41d850a8a833
stephen-j-oleary <s.james.oleary@gmail.com>
2022-11-20 19:00:57 -0700

    Add variant and color props for styling

--- components/Button/index.js
+++ components/Button/index.js
@@ -1,17 +1,84 @@
 
 import styles from "./styles.module.scss";
-import { forwardRef } from "react";
+import { forwardRef, useState } from "react";
 import classNames from "classnames";
 
 import withTooltip from "../withTooltip/index.js";
+import _ from "lodash";
 
-const Button = forwardRef(function Button({ className, type = "button", children, ...props }, ref) {
+const COLORS = {
+  primary: ["50 90 205", "250 250 255", "150 150 160"]
+};
+
+export const RadioButton = ({
+  options = [],
+  value,
+  initialValue,
+  onChange = _.noop,
+  variant = "primary",
+  color = "primary",
+  ...props
+}) => {
+  const [currentValue, setCurrentValue] = useState(value || initialValue || options[0].value);
+
+  const handleClick = value => {
+    setCurrentValue(value);
+    onChange(value);
+  }
+
+  return (
+    <div
+      className={classNames(
+        props.className,
+        styles.radioButton,
+      )}
+      style={{
+        "--color1": COLORS[color][0],
+        "--color2": COLORS[color][1],
+        "--color3": COLORS[color][2]
+      }}
+    >
+      {
+        options.map(item => (
+          <Button
+            key={item.value}
+            className={classNames({ selected: (currentValue === item.value) })}
+            variant={variant}
+            color={color}
+            onClick={() => handleClick(item.value)}
+          >
+            {item.label}
+          </Button>
+        ))
+      }
+    </div>
+  );
+}
+
+const Button = forwardRef(function Button({
+  type = "button",
+  size = "sm",
+  variant = "primary",
+  color = "primary",
+  children,
+  ...props
+}, ref) {
   return (
     <button
-      ref={ref}
-      className={classnames(className, styles.button)}
-      type={type}
       {...props}
+      ref={ref}
+      className={classNames(
+        props.className,
+        styles.button,
+        styles[variant],
+        ...size.split("-").map(c => styles[c])
+      )}
+      style={{
+        "--color1": COLORS[color][0],
+        "--color2": COLORS[color][1],
+        "--color3": COLORS[color][2]
+      }}
+      type={type}
     >
       {children}
     </button>
