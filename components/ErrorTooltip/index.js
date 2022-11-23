01a3fbabce82c8bdbc6c71457313c611ad921e48
stephen-j-oleary <s.james.oleary@gmail.com>
2022-11-23 15:55:59 -0700

    Change how default props are added Avoid potential mutation of arguments

--- components/ErrorTooltip/index.js
+++ components/ErrorTooltip/index.js
@@ -5,15 +5,19 @@ import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 import Tooltip from "react-bootstrap/Tooltip";
 
 export default function ErrorTooltip({ error, children, ...props }) {
-  _.defaultsDeep(props, {
-    placement: "bottom",
-    delay: { show: 0, hide: 300 },
-    overlay: <Tooltip>{error}</Tooltip>,
-    trigger: error ? ["hover", "focus"] : []
-  });
+  const _props = _.defaultsDeep(
+    {},
+    props,
+    {
+      placement: "bottom",
+      delay: { show: 0, hide: 300 },
+      overlay: <Tooltip>{error}</Tooltip>,
+      trigger: error ? ["hover", "focus"] : []
+    }
+  );
 
   return (
-    <OverlayTrigger {...props}>
+    <OverlayTrigger {..._props}>
       {children}
     </OverlayTrigger>
   );
