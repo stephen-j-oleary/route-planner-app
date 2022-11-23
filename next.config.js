7d08b839159eaf7488900cfcdb24333f37627952
stephen-j-oleary <s.james.oleary@gmail.com>
2022-11-22 20:27:02 -0700

    Add Sentry error tracker

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
-module.exports = {
+
+const { withSentryConfig } = require('@sentry/nextjs');
+
+const moduleExports = {
   env: {
     NEXT_PUBLIC_GOOGLE_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_API_KEY
+  },
+  sentry: {
+    hideSourceMaps: true
   }
 }
+
+const sentryWebpackPluginOptions = {
+  silent: true
+}
+
+module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
