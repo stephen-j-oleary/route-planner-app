33a4a2006f3f19d890209b6fbf2099bafec9dcc7
stephen-j-oleary <s.james.oleary@gmail.com>
2022-11-23 20:46:25 -0700

    Fix aws env variables not accesible in next routes

--- next.config.js
+++ next.config.js
@@ -3,7 +3,15 @@ const { withSentryConfig } = require('@sentry/nextjs');
 
 const moduleExports = {
   env: {
-    NEXT_PUBLIC_GOOGLE_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_API_KEY
+    NEXT_PUBLIC_GOOGLE_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_API_KEY,
+    DIRECTIONS_API_URL: process.env.DIRECTIONS_API_URL,
+    SEARCH_API_URL: process.env.SEARCH_API_URL,
+    RAPID_API_HOST: process.env.RAPID_API_HOST,
+    RAPID_API_KEY: process.env.RAPID_API_KEY,
+    SENTRY_AUTH_TOKEN: process.env.SENTRY_AUTH_TOKEN,
+    SENTRY_ORG: process.env.SENTRY_ORG,
+    SENTRY_PROJECT: process.env.SENTRY_PROJECT,
+    SENTRY_URL: process.env.SENTRY_URL
   },
   sentry: {
     hideSourceMaps: true
