581867d218ce796d1f239d8d8cc66d8ad77567fd
stephen-j-oleary <s.james.oleary@gmail.com>
2023-01-10 16:06:56 -0700

    Add hide map toggle for mobile

--- components/Map/index.js
+++ components/Map/index.js
@@ -1,20 +1,25 @@
 
 import styles from "./styles.module.css";
-import classNames from "classnames";
 import { useSelector, useDispatch } from "react-redux";
-import { setState, selectIsState, selectOptions, mergeOptions, /* selectMarkers, selectPolylines, setHoveredMarker, setSelectedMarker, */ selectMarkup, setHoveredMarkup, setSelectedMarkup } from "../../redux/slices/map.js";
+import { setState, selectIsState, selectOptions, mergeOptions, selectMarkup, setHoveredMarkup, setSelectedMarkup, toggleHideMap, selectHideMap } from "../../redux/slices/map.js";
 
 import LoadingDots from "../LoadingDots";
 import GoogleMap from "../Google/Map";
 import { useCallback } from "react";
+import { FaMap } from "react-icons/fa";
+import { useTheme } from "@mui/material/styles";
+import Button from "../Button";
+import { Box, Stack, useMediaQuery } from "@mui/material";
 
 export default function Map(props) {
+  const theme = useTheme();
+  const isDesktop = useMediaQuery(theme.breakpoints.up("md"));
+
   const isLoading = useSelector(state => selectIsState(state, "loading"));
   const { center, zoom, heading, ...options } = useSelector(selectOptions);
   const markup = useSelector(selectMarkup);
-  /* const markers = useSelector(selectMarkers);
-  const polylines = useSelector(selectPolylines); */
   const dispatch = useDispatch();
+  const hideMap = useSelector(selectHideMap);
 
   // Map handlers
   const handleMapLoad = useCallback(
@@ -50,46 +55,76 @@ export default function Map(props) {
     [dispatch]
   );
 
+  const toggleMapView = () => {
+    dispatch(toggleHideMap());
+  };
+
   return (
-    <div
+    <Box
+      flex={{ md: 2 }}
+      height={!isDesktop ? (hideMap ? "calc(36px + 2rem)" : "50vh") : "auto"}
+      minWidth="400px"
+      sx={{
+        transition: "height .25s ease-in-out",
+        background: theme.palette.background.default
+      }}
       {...props}
-      className={classNames(
-        props.className,
-        styles.container
-      )}
     >
-      {isLoading && <CompPlaceholder />}
-      <GoogleMap
-        className={styles.map}
-        center={center}
-        zoom={zoom}
-        heading={heading}
-        options={options}
-        markup={{
-          items: markup,
-          listeners: {
-            mouseover: handleMarkupMouseOver,
-            mouseout: handleMarkupMouseOut,
-            click: handleMarkupClick
-          }
+      <Box
+        className={!isDesktop && (hideMap ? styles.hideMap : styles.showMap)}
+        width="100%"
+        height="100%"
+        sx={{
+          position: "relative"
         }}
-        listeners={{
-          tilesloaded: handleMapLoad,
-          idle: handleMapIdle
-        }}
-      />
-    </div>
+      >
+        <GoogleMap
+          className={styles.map}
+          center={center}
+          zoom={zoom}
+          heading={heading}
+          options={options}
+          markup={{
+            items: markup,
+            listeners: {
+              mouseover: handleMarkupMouseOver,
+              mouseout: handleMarkupMouseOut,
+              click: handleMarkupClick
+            }
+          }}
+          listeners={{
+            tilesloaded: handleMapLoad,
+            idle: handleMapIdle
+          }}
+        />
+        {
+          isLoading && (
+            <Stack
+              justifyContent="center"
+              alignItems="center"
+              sx={{
+                background: `rgb(${theme.palette.background.defaultChannel} / .5)`
+              }}
+              className={styles.map}
+            >
+              <LoadingDots />
+              Loading Map...
+            </Stack>
+          )
+        }
+      </Box>
+
+      <Button
+        variant="contained"
+        className={styles.hideMapBtn}
+        onClick={toggleMapView}
+        startIcon={<FaMap />}
+        sx={isDesktop ? {
+          display: "none"
+        } : {}}
+      >
+        {hideMap ? "Show" : "Hide"}
+      </Button>
+    </Box>
   )
 }
-
-const CompPlaceholder = () => (
-  <div
-    className={classNames(
-      styles.map,
-      styles.placeholder
-    )}
-  >
-    <LoadingDots />
-    Loading Map...
-  </div>
-)
