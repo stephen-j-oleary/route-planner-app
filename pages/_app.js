70b0385bd708412f22f0a944c19d5d741d1098ac
stephen-j-oleary <s.james.oleary@gmail.com>
2023-04-19 21:10:43 -0600

    Remove unused ErrorFallback component

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,6 @@ import "@/shared/styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { store } from "../redux/store.js";
 import ErrorBoundary from "@/components/ErrorBoundary";
-import ErrorFallback from "../components/ErrorFallback";
 import { Provider } from "react-redux";
 import Head from "next/head";
 
