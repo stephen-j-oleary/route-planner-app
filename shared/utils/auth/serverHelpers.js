3d6bd8808bedd356c7045e7a306dd2d11fbca792
stephen-j-oleary <s.james.oleary@gmail.com>
2023-04-24 15:04:53 -0600

    Add next-auth helpers module

---
+++ shared/utils/auth/serverHelpers.js
@@ -0,0 +1,19 @@
+import { getServerSession } from "next-auth/next";
+
+import { authOptions } from "../../../pages/api/auth/[...nextauth]";
+
+
+export async function getAuthSession(req, res) {
+  const session = await getServerSession(req, res, authOptions);
+  return session;
+}
+
+export async function getAuthScope(req, res) {
+  const session = await getAuthSession(req, res);
+  return session ? "private" : "public";
+}
+
+export async function getAuthUser(req, res) {
+  const session = await getAuthSession(req, res);
+  return session?.user;
+}
