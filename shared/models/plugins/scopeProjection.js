97797dddce7f673b9c142c92f0e92f1a3f658e26
stephen-j-oleary <s.james.oleary@gmail.com>
2023-04-24 15:08:44 -0600

    Add mongoose helpers and plugins

---
+++ shared/models/plugins/scopeProjection.js
@@ -0,0 +1,25 @@
+export default function getScopeProjection(
+  scope = [],
+  schema,
+  {
+    includeUnscoped = false
+  } = {}
+) {
+  const scopeArr = (typeof scope === "string") ? [scope] : scope;
+  if (scopeArr.length === 0) return null;
+
+  const props = [];
+  for (const [pathname, value] of Object.entries(schema.tree)) {
+    const shouldInclude = () => {
+      if (value.unselect) return false;
+      if (pathname === "_id") return true;
+      if (includeUnscoped && !value.scope) return true;
+      if (scopeArr.some(sc => (value.scope === sc || value.scope?.includes?.(sc)))) return true;
+      return false;
+    };
+
+    if (shouldInclude()) props.push(pathname);
+  }
+
+  return props;
+}
