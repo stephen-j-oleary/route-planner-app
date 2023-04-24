b650497bad81654e1340e7652f5ade8b02db1dfa
stephen-j-oleary <s.james.oleary@gmail.com>
2023-04-24 15:54:16 -0600

    Add user avatar logic hook

---
+++ shared/hooks/useUserAvatar/index.js
@@ -0,0 +1,22 @@
+import { useCallback } from "react";
+
+import { selectUser, useGetSession } from "@/shared/reactQuery/useSession";
+
+
+export default function useUserAvatar({ size = 40 } = {}) {
+  const authUser = useGetSession({ select: selectUser });
+
+  const getProps = useCallback(
+    () => ({
+      src: authUser.data?.image,
+      children: authUser.data?.email?.slice(0, 1),
+      sx: {
+        width: size,
+        height: size,
+      },
+    }),
+    [authUser, size]
+  );
+
+  return { getProps };
+}
