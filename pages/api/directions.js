b452a0891ac6f2fe53a1aa6252d68f6770a2ecd3
stephen-j-oleary <s.james.oleary@gmail.com>
2023-04-25 15:26:56 -0600

    Refactor directions and search api endpoints

--- pages/api/directions.js
+++ pages/api/directions.js
@@ -1,28 +1,44 @@
-
-import axios from "axios";
+import { handleGetSubscriptions } from "@/pages/api/pay/subscriptions";
+import nextConnect from "@/shared/nextConnect";
+import { createUsageRecord } from "@/shared/services/usageRecords";
+import { getAuthUser } from "@/shared/utils/auth/serverHelpers";
+import httpClient from "@/shared/utils/httpClient";
 
 const URL = process.env.DIRECTIONS_API_URL;
 const API_KEY = process.env.RAPID_API_KEY;
 const API_HOST = process.env.RAPID_API_HOST;
 
-export default async function handler(req, res) {
-  const config = {
+
+const handler = nextConnect();
+
+handler.get(async (req, res) => {
+  const { query } = req;
+
+  const authUser = await getAuthUser(req, res);
+  if (!authUser?.customerId) throw { status: 401, message: "Not authorized" };
+
+  const subscriptions = await handleGetSubscriptions({ customer: authUser.customerId });
+  if (!subscriptions) throw { status: 401, message: "Not authorized" };
+
+  const subscriptionItem = subscriptions[0].items.data[0];
+  if (!subscriptionItem) throw { status: 401, message: "Not authorized" };
+
+  const { data } = await httpClient.request({
     method: "get",
     url: URL,
     headers: {
       "X-RapidAPI-Key": API_KEY,
       "X-RapidAPI-Host": API_HOST
     },
-    params: req.query
-  };
+    params: query
+  });
 
-  try {
-    const response = await axios.request(config);
-    res.status(200).json(response.data);
-  }
-  catch (err) {
-    console.error(err);
-    const { response } = err;
-    return res.status(response?.status || 500).json(response?.data || err.message);
-  }
-}
+  await createUsageRecord({
+    subscriptionItem: subscriptionItem.id,
+    quantity: 1,
+  });
+
+  res.status(200).json(data);
+});
+
+export default handler;
