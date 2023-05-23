3c44f6e2c05f8a8b04d0207ee7cef190cc1ee9bc
stephen-j-oleary <s.james.oleary@gmail.com>
2023-05-23 16:10:43 -0600

    Change next.config to include all env variables

--- next.config.mjs
+++ next.config.mjs
@@ -2,15 +2,7 @@
 import NextBundleAnalyzer from "@next/bundle-analyzer";
 
 const moduleExports = {
-  env: {
-    NEXT_PUBLIC_GOOGLE_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_API_KEY,
-    DIRECTIONS_API_URL: process.env.DIRECTIONS_API_URL,
-    SEARCH_API_URL: process.env.SEARCH_API_URL,
-    AUTOCOMPLETE_API_URL: process.env.AUTOCOMPLETE_API_URL,
-    RAPID_API_HOST: process.env.RAPID_API_HOST,
-    RAPID_API_KEY: process.env.RAPID_API_KEY,
-    NEXT_PUBLIC_ANALYTICS_MEASUREMENT_ID: process.env.NEXT_PUBLIC_ANALYTICS_MEASUREMENT_ID
-  },
+  env: process.env.NODE_ENV === "production" ? process.env : {},
   modularizeImports: {
     lodash: {
       transform: "lodash/{{member}}",
