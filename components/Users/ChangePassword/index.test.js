caa1fe532f046206f04a29dcc3d731146207e391
stephen-j-oleary <s.james.oleary@gmail.com>
2023-06-08 15:06:01 -0600

    Add unit tests

---
+++ components/Users/ChangePassword/index.test.js
@@ -0,0 +1,104 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import ChangePassword from ".";
+import createUseDeferredMock from "@/__utils__/createUseDeferredMock";
+import useDeferred from "@/shared/hooks/useDeferred";
+import { useUpdateAccountCredentialsById } from "@/shared/reactQuery/useAccounts";
+
+jest.mock("@/shared/reactQuery/useAccounts");
+jest.mock("@/shared/hooks/useDeferred");
+
+const VALID_PASSWORD = "ValidPassword1";
+
+
+describe("ChangePassword", () => {
+  afterEach(jest.clearAllMocks);
+
+  it("is a button", () => {
+    render(<ChangePassword />);
+
+    expect(screen.getByRole("button")).toBeInTheDocument();
+  });
+
+  it("shows a form dialog", async () => {
+    render(<ChangePassword />);
+
+    await userEvent.click(screen.getByRole("button", { name: /change/i }));
+
+    await waitFor(() => {
+      expect(screen.getByRole("dialog")).toBeInTheDocument();
+    });
+  });
+
+  it("calls change password when confirmed", async () => {
+    render(<ChangePassword />);
+
+    await userEvent.click(screen.getByRole("button", { name: /change/i }));
+    await screen.findByRole("dialog"); // Wait for the dialog to show
+    await userEvent.type(screen.getByLabelText(/current/i), VALID_PASSWORD);
+    await userEvent.type(screen.getByLabelText(/new/i), VALID_PASSWORD);
+    await userEvent.click(screen.getByRole("button", { name: /change/i }));
+
+    expect(useUpdateAccountCredentialsById().mutate).toBeCalledTimes(1);
+  });
+
+  it("passes the correct data on submit", async () => {
+    render(<ChangePassword />);
+
+    await userEvent.click(screen.getByRole("button", { name: /change/i }));
+    await screen.findByRole("dialog"); // Wait for the dialog to show
+    await userEvent.type(screen.getByLabelText(/current/i), VALID_PASSWORD);
+    await userEvent.type(screen.getByLabelText(/new/i), VALID_PASSWORD);
+    await userEvent.click(screen.getByRole("button", { name: /change/i }));
+
+    expect(useUpdateAccountCredentialsById().mutate).toBeCalledWith(
+      expect.objectContaining({
+        id: "id",
+        oldCredentials: {
+          email: "email",
+          password: VALID_PASSWORD,
+        },
+        email: "email",
+        password: VALID_PASSWORD,
+      }),
+      expect.anything()
+    );
+  });
+
+  it("does not call change password when cancelled", async () => {
+    render(<ChangePassword />);
+
+    await userEvent.click(screen.getByRole("button", { name: /change/i }));
+    await screen.findByRole("dialog"); // Wait for the dialog to show
+    await userEvent.type(screen.getByLabelText(/current/i), VALID_PASSWORD);
+    await userEvent.type(screen.getByLabelText(/new/i), VALID_PASSWORD);
+    await userEvent.click(screen.getByRole("button", { name: /cancel/i }));
+
+    expect(useUpdateAccountCredentialsById().mutate).not.toBeCalled();
+  });
+
+  it("inputs and submit are disabled when loading default values", async () => {
+    useDeferred.mockImplementationOnce(createUseDeferredMock());
+
+    render(<ChangePassword />);
+
+    await userEvent.click(screen.getByRole("button", { name: /change/i }));
+    await screen.findByRole("dialog"); // Wait for the dialog to show
+
+    expect(screen.getByLabelText(/current/i)).toBeDisabled();
+    expect(screen.getByLabelText(/new/i)).toBeDisabled();
+    expect(screen.getByRole("button", { name: /change/i })).toBeDisabled();
+  });
+
+  it("submit is disabled when submitting", async () => {
+    useUpdateAccountCredentialsById.mockReturnValue({ isLoading: true });
+
+    render(<ChangePassword />);
+
+    await userEvent.click(screen.getByRole("button", { name: /change/i }));
+    await screen.findByRole("dialog"); // Wait for the dialog to show
+
+    expect(screen.getByRole("button", { name: /change/i })).toBeDisabled();
+  });
+});
