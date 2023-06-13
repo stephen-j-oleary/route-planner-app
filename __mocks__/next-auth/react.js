2d16cc09f6ba99913f56c8a6c55908af48e4f389
stephen-j-oleary <s.james.oleary@gmail.com>
2023-06-13 16:21:58 -0600

    Add missing mocks

--- __mocks__/next-auth/react.js
+++ __mocks__/next-auth/react.js
@@ -3,4 +3,8 @@ export const useSession = jest.fn(() => ({
   data: {},
 }));
 
+export const getProviders = jest.fn(async () => ({}));
+
+export const signIn = jest.fn();
+
 export const signOut = jest.fn();
