1523c7535a6082d33ea06c69e8f5594c2a4b2211
stephen-j-oleary <s.james.oleary@gmail.com>
2023-06-14 16:51:46 -0600

    Update mock implementation

--- shared/reactQuery/__mocks__/useSession.js
+++ shared/reactQuery/__mocks__/useSession.js
@@ -1,7 +1,12 @@
-export const useGetSession = jest.fn().mockReturnValue({
-  isFetched: true,
-  data: {
-    image: "",
-    email: "",
-  },
-});
+import createUseQueryMock from "@/__utils__/createUseQueryMock";
+
+export const useGetSession = jest.fn().mockImplementation(
+  createUseQueryMock({
+    status: "success",
+    data: {
+      _id: "id",
+      image: "https://images.com/image",
+      email: "example@email.com",
+    },
+  })
+);
