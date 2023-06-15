943fb419114be1c655bed369697bc1de38d6b53e
stephen-j-oleary <s.james.oleary@gmail.com>
2023-06-14 19:52:00 -0600

    Improve jsdocs

--- __utils__/createUseFormMock.js
+++ __utils__/createUseFormMock.js
@@ -2,7 +2,7 @@ const { useForm } = jest.requireActual("react-hook-form");
 
 /**
  * Returns a mock instance of useForm that will hold the given formState, values and errors
- * @param {*} state The mocked state of the form
+ * @param {{ optionReplacements: import("react-hook-form").UseFormProps } & import("react-hook-form").UseFormReturn} state The mocked state of the form
  * @returns {import("react-hook-form").UseFormReturn}
  */
 export default function createUseFormMock({ optionReplacements = {}, ...replacements } = {}) {
