cf97593d55b21d43e0c50991d4a12be57ddeb9d4
stephen-j-oleary <s.james.oleary@gmail.com>
2023-06-21 21:35:43 -0600

    Add integration tests

---
+++ components/Routes/SaveRoute/index.int.test.js
@@ -0,0 +1,40 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import SaveRoute from ".";
+import QueryClientProvider from "@/shared/providers/QueryClientProvider";
+import httpClient from "@/shared/utils/httpClient";
+
+jest.unmock("react-query");
+jest.mock("@/shared/utils/httpClient", () => ({
+  request: jest.fn(),
+}));
+
+const ROUTE = {
+  name: "Route",
+};
+
+
+const wrapper = props => <QueryClientProvider {...props} />;
+
+const getButton = () => screen.getByRole("button", { name: /save route/i });
+
+describe("SaveRoute", () => {
+  it("properly handles saving the route", async () => {
+    render(
+      <SaveRoute route={ROUTE} />,
+      { wrapper }
+    );
+
+    await waitFor(() => {
+      expect(getButton()).toBeInTheDocument();
+    });
+    await userEvent.click(getButton());
+
+    expect(httpClient.request).toBeCalledWith({
+      url: "api/routes",
+      method: expect.stringMatching(/post/i),
+      data: ROUTE,
+    });
+  });
+});
---
+++ components/Routes/UnsaveRoute/index.int.test.js
@@ -0,0 +1,40 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import UnsaveRoute from ".";
+import QueryClientProvider from "@/shared/providers/QueryClientProvider";
+import httpClient from "@/shared/utils/httpClient";
+
+
+jest.unmock("react-query");
+jest.mock("@/shared/utils/httpClient", () => ({
+  request: jest.fn(),
+}));
+
+const ROUTE = {
+  _id: "route_id",
+};
+
+
+const wrapper = props => <QueryClientProvider {...props} />;
+
+const getButton = () => screen.getByRole("button", { name: /unsave route/i });
+
+describe("UnsaveRoute", () => {
+  it("properly handles unsaving a saved route", async () => {
+    render(
+      <UnsaveRoute route={ROUTE} />,
+      { wrapper }
+    );
+
+    await waitFor(() => {
+      expect(getButton()).toBeInTheDocument();
+    });
+    await userEvent.click(getButton());
+
+    expect(httpClient.request).toBeCalledWith({
+      url: `api/routes/${ROUTE._id}`,
+      method: expect.stringMatching(/delete/i),
+    });
+  });
+});
