58a998d9e19186210f58e34740c215ceb1df6e6c
stephen-j-oleary <s.james.oleary@gmail.com>
2023-11-24 22:05:14 -0700

    Add getLayout types

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,31 @@
 import "@/shared/styles/globals.css";
+import { EmotionCache } from "@emotion/cache";
+import { NextPage } from "next";
 import Head from "next/head";
+import { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import React from "react";
 
 import ErrorBoundary from "@/components/ErrorBoundary";
 import EmotionCacheProvider from "@/shared/providers/EmotionCacheProvider";
 import QueryClientProvider from "@/shared/providers/QueryClientProvider";
 import ThemeProvider from "@/shared/providers/ThemeProvider";
 
+
+export type { NextPage } from "next";
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (layoutProps: { children: React.ReactNode }) => React.ReactNode,
+};
+
+export type AppProps = {
+  Component: NextPage | NextPageWithLayout,
+  emotionCache: EmotionCache,
+  pageProps: {
+    [x: string]: any,
+    session: Session,
+  },
+};
 
 export default function App({
   Component,
@@ -15,8 +34,9 @@ export default function App({
     session,
     ...pageProps
   }
-}) {
-  const { getLayout = (page => page) } = Component;
+}: AppProps) {
+  const getLayout = ("getLayout" in Component ? Component.getLayout : undefined)
+    || (({ children }) => children);
 
   return (
     <EmotionCacheProvider emotionCache={emotionCache}>
