82fe8cb2330c0f99a6bd1ceaaa4f2bac0045b463
stephen-j-oleary <s.james.oleary@gmail.com>
2023-11-29 14:52:48 -0700

    Add boolean param handling

--- shared/nextConnect/middleware/parseQuery.ts
+++ shared/nextConnect/middleware/parseQuery.ts
@@ -1,12 +1,12 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { NextHandler } from "next-connect";
-import qs, { ParsedQs } from "qs";
+import queryString from "query-string";
 
 
-export default function parseQuery(req: Omit<NextApiRequest, "query"> & { query: ParsedQs }, _res: NextApiResponse, next: NextHandler) {
+export default function parseQuery(req: Omit<NextApiRequest, "query"> & { query: queryString.ParsedQuery<string | boolean> }, _res: NextApiResponse, next: NextHandler) {
   if (req.query) {
-    const queryString = qs.stringify(req.query);
-    req.query = qs.parse(queryString);
+    const qs = queryString.stringify(req.query);
+    req.query = queryString.parse(qs, { parseBooleans: true, arrayFormat: "bracket" });
   }
   next();
 }
