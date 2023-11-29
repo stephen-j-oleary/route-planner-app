a8df571c80222411c4b992624055ee0131f2eb06
stephen-j-oleary <s.james.oleary@gmail.com>
2023-11-28 18:01:39 -0700

    Add helper for retrieving the authorized customer

--- shared/utils/auth/serverHelpers.ts
+++ shared/utils/auth/serverHelpers.ts
@@ -1,6 +1,8 @@
 import { getServerSession } from "next-auth/next";
 
 import { getNextAuthOptions } from "@/pages/api/auth/[...nextauth]";
+import { handleGetCustomerById } from "@/pages/api/pay/customers/[customerId]";
+import { handleGetUserById } from "@/pages/api/users/[id]";
 import { NextRequest, NextResponse } from "@/types/next";
 
 
@@ -14,6 +16,18 @@ export async function getAuthScope(req: NextRequest, res: NextResponse) {
 }
 
 export async function getAuthUser(req: NextRequest, res: NextResponse) {
-  const session = await getAuthSession(req, res);
-  return session?.user;
+  const { user: { _id } } = await getAuthSession(req, res);
+
+  return await handleGetUserById(_id);
+}
+
+export async function getAuthCustomer(req: NextRequest, res: NextResponse) {
+  const authUser = await getAuthUser(req, res);
+  let { _id, customerId } = authUser || {};
+  if (!_id && !customerId) return undefined;
+
+  customerId ??= (await handleGetUserById(_id))?.customerId;
+  if (!customerId) return undefined;
+
+  return await handleGetCustomerById(customerId);
 }
