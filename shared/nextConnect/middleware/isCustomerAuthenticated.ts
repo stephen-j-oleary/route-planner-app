68c73e34125828872327a4814ddbe61e14af84ca
stephen-j-oleary <s.james.oleary@gmail.com>
2023-11-28 18:05:54 -0700

    Add db check for incomplete session object

--- shared/nextConnect/middleware/isCustomerAuthenticated.ts
+++ shared/nextConnect/middleware/isCustomerAuthenticated.ts
@@ -1,13 +1,17 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { NextHandler } from "next-connect";
 
+import { handleGetUserById } from "@/pages/api/users/[id]";
 import { AuthError } from "@/shared/utils/ApiErrors";
 import { getAuthUser } from "@/shared/utils/auth/serverHelpers";
 
 
 export default async function isCustomerAuthenticated(req: NextApiRequest, res: NextApiResponse, next: NextHandler) {
   const authUser = await getAuthUser(req, res);
-  if (!authUser?.customerId) throw new AuthError("Missing or invalid customer authentication");
+  if (authUser?.customerId) return next();
 
-  next();
+  const user = await handleGetUserById(authUser?._id);
+  if (user?.customerId) return next();
+
+  throw new AuthError("Missing or invalid customer authentication");
 }
