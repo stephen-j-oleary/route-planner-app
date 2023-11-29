353bca4639b9a559421afdf300fa6bed2820b21e
stephen-j-oleary <s.james.oleary@gmail.com>
2023-11-28 17:38:01 -0700

    Redirect to callback url if set when logged in

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,3 +1,4 @@
+import { isArray } from "lodash";
 import { GetServerSidePropsContext } from "next";
 
 import { Box, Container } from "@mui/material";
@@ -8,10 +9,13 @@ import { NextPageWithLayout } from "@/pages/_app";
 import { getAuthUser } from "@/shared/utils/auth/serverHelpers";
 
 
-export async function getServerSideProps({ req, res }: GetServerSidePropsContext) {
+export async function getServerSideProps({ req, res, query }: GetServerSidePropsContext) {
+  let { callbackUrl = "/account" } = query;
+  if (isArray(callbackUrl)) callbackUrl = callbackUrl[0];
+
   const USER_LOGGED_IN_REDIRECT = {
     redirect: {
-      destination: "/account",
+      destination: callbackUrl,
       permanent: false,
     },
   };
