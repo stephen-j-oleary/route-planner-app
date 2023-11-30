e3f3249ed1d27c9467beb4963dfd2d099c8198b1
stephen-j-oleary <s.james.oleary@gmail.com>
2023-11-30 16:49:25 -0700

    Fix getAuthUser return when signed out

--- shared/utils/auth/serverHelpers.ts
+++ shared/utils/auth/serverHelpers.ts
@@ -19,8 +19,10 @@ export async function getAuthScope(req: NextRequest, res: NextResponse) {
 
 export async function getAuthUser(req: NextRequest, res: NextResponse) {
   const session = await getAuthSession(req, res);
+  if (!session?.user?.id) return null;
 
   const user = await handleGetUserById(session?.user?.id);
+  if (!user) return null;
   return fromMongoose<User>(user);
 }
 
