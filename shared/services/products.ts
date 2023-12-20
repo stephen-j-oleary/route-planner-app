e66ae409addf15eda6bd475a8218dd4f1f819f26
stephen-j-oleary <s.james.oleary@gmail.com>
2023-12-19 20:17:55 -0700

    Migrate to ts

---
+++ shared/services/products.ts
@@ -0,0 +1,32 @@
+import { ApiGetProductsQuery, ApiGetProductsResponse } from "@/pages/api/pay/products";
+import { ApiGetProductByIdQuery, ApiGetProductByIdResponse } from "@/pages/api/pay/products/[id]";
+import httpClient from "@/shared/utils/httpClient";
+
+const BASE_PATH = "api/pay/products";
+
+
+export type GetProductsParams = ApiGetProductsQuery;
+export type GetProductsReturn = Awaited<ReturnType<typeof getProducts>>;
+
+export async function getProducts(params: GetProductsParams = {}) {
+  const { data } = await httpClient.request<ApiGetProductsResponse>({
+    method: "get",
+    url: BASE_PATH,
+    params,
+  });
+
+  return data;
+}
+
+export type GetProductByIdParams = Omit<ApiGetProductByIdQuery, "id">;
+export type GetProductByIdReturn = Awaited<ReturnType<typeof getProductById>>;
+
+export async function getProductById(id: string, params: GetProductByIdParams = {}) {
+  const { data } = await httpClient.request<ApiGetProductByIdResponse>({
+    method: "get",
+    url: `${BASE_PATH}/${id}`,
+    params,
+  });
+
+  return data;
+}
