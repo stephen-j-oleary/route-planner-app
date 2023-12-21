eef8818145e1864eac8715d756f1ede125645650
stephen-j-oleary <s.james.oleary@gmail.com>
2023-12-20 18:39:43 -0700

    Fix mocks

--- shared/reactQuery/__mocks__/useSession.ts
+++ shared/reactQuery/__mocks__/useSession.ts
@@ -1,15 +1,18 @@
 import createUseQueryMock from "@/__utils__/createUseQueryMock";
-import { IUser } from "@/shared/models/User";
+
+export const { selectUser, selectCustomerId } = jest.requireActual("@/shared/reactQuery/useSession");
 
 export const useGetSession = jest.fn().mockImplementation(
-  createUseQueryMock<IUser>("success", {
+  createUseQueryMock("success", {
     data: {
-      _id: "id",
-      id: "id",
-      image: "https://images.com/image",
-      customerId: "customerId",
-      email: "example@email.com",
-      emailVerified: null,
+      user: {
+        _id: "id",
+        id: "id",
+        image: "https://images.com/image",
+        customerId: "customerId",
+        email: "example@email.com",
+        emailVerified: null,
+      },
     },
   })
 );
