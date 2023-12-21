287db6b4179190ab469c37e9f0eaa5c19be491a0
stephen-j-oleary <s.james.oleary@gmail.com>
2023-12-20 18:56:13 -0700

    Add integration test

---
+++ __tests__/pages/subscribe.int.test.tsx
@@ -0,0 +1,58 @@
+jest.mock("@/shared/utils/auth/serverHelpers", () => ({
+  getAuthUser: jest.fn(),
+}));
+
+import { render, screen } from "@testing-library/react";
+
+import createGetServerSidePropsContext from "@/__utils__/createGetServerSidePropsContext";
+import Page, { getServerSideProps } from "@/pages/subscribe/[...slug]";
+import QueryClientProvider from "@/shared/providers/QueryClientProvider";
+import { getAuthUser } from "@/shared/utils/auth/serverHelpers";
+
+const mockedGetAuthUser = getAuthUser as jest.Mock;
+
+
+const wrapper = QueryClientProvider;
+
+describe("/subscribe", () => {
+  mockedGetAuthUser.mockResolvedValue({ id: "user" });
+
+  afterEach(() => jest.clearAllMocks());
+
+  it("handles logged out", async () => {
+    const resolvedUrl = "/current/page";
+    mockedGetAuthUser.mockResolvedValueOnce(null);
+    const result = await getServerSideProps(createGetServerSidePropsContext({ resolvedUrl }));
+
+    expect(result).toHaveProperty("redirect", expect.objectContaining({
+      destination: `/register?callbackUrl=${resolvedUrl}`,
+      permanent: false,
+    }));
+  })
+
+  it("server handles missing or invalid query params", async () => {
+    const result = await getServerSideProps(createGetServerSidePropsContext({ params: { slug: [] } }));
+
+    expect(result).toHaveProperty("notFound", true);
+  })
+
+  it("server handles price id param", async () => {
+    const priceId = "price_id";
+    const result = await getServerSideProps(createGetServerSidePropsContext({ params: { slug: ["id", priceId] } }));
+
+    expect(result).toHaveProperty("props", expect.objectContaining({ priceId }));
+  })
+
+  it("server handles lookup key param", async () => {
+    const lookupKey = "lookup_key";
+    const result = await getServerSideProps(createGetServerSidePropsContext({ params: { slug: [lookupKey] } }));
+
+    expect(result).toHaveProperty("props", expect.objectContaining({ lookupKey }));
+  })
+
+  it("renders the subscribe form", () => {
+    render(<Page />, { wrapper });
+
+    expect(screen.getByRole("form", { busy: true })).toBeVisible();
+  })
+})
