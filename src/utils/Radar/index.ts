b12297340988823bea66e07e3edddab444eb3ffb
stephen-j-oleary <s.james.oleary@gmail.com>
2024-01-24 23:47:11 -0700

    Add Radar utility package

---
+++ src/utils/Radar/index.ts
@@ -0,0 +1,91 @@
+import { AxiosError } from "axios";
+
+import { AutocompleteParams, AutocompleteResponse, DirectionsParams, DirectionsResponse, GeocodeParams, GeocodeResponse, MatrixParams, MatrixResponse } from "./types";
+import httpClient from "@/utils/httpClient";
+
+const RADAR_API = process.env.LOOP_RADAR_API;
+const RADAR_PK = process.env.LOOP_RADAR_PK;
+
+
+export type {
+  Address,
+  AutocompleteParams,
+  AutocompleteResponse,
+  DirectionsParams,
+  DirectionsResponse,
+  GeocodeParams,
+  GeocodeResponse,
+  MatrixParams,
+  MatrixResponse,
+} from "./types";
+
+export default class Radar {
+  pk: string;
+  api: string;
+
+  constructor() {
+    if (!RADAR_PK) throw new Error("Radar: Missing private key");
+    if (!RADAR_API) throw new Error("Radar: Missing api location");
+
+    this.pk = RADAR_PK;
+    this.api = RADAR_API;
+  }
+
+  async autocomplete(params: AutocompleteParams) {
+    const { data } = await httpClient.request<AutocompleteResponse>({
+      method: "get",
+      url: `${this.api}/search/autocomplete`,
+      headers: { "Authorization": this.pk },
+      params,
+    });
+
+    return data;
+  }
+
+  async directions(params: DirectionsParams) {
+    try {
+      const { data } = await httpClient.request<DirectionsResponse>({
+        method: "get",
+        url: `${this.api}/route/directions`,
+        headers: { "Authorization": this.pk },
+        params: {
+          ...params,
+          units: params.units || "metric",
+          mode: "car",
+        },
+      });
+
+      return data.routes;
+    }
+    catch (err: unknown) {
+      if (err instanceof AxiosError) throw new Error(err.response?.data.message);
+      throw err;
+    }
+  }
+
+  async geocode(params: GeocodeParams) {
+    const { data } = await httpClient.request<GeocodeResponse>({
+      method: "get",
+      url: `${this.api}/geocode/forward`,
+      headers: { "Authorization": this.pk },
+      params,
+    });
+
+    return data;
+  }
+
+  async matrix(params: MatrixParams) {
+    const { data } = await httpClient.request<MatrixResponse>({
+      method: "get",
+      url: `${this.api}/route/matrix`,
+      headers: { "Authorization": this.pk },
+      params: {
+        ...params,
+        units: params.units || "metric",
+        mode: "car",
+      },
+    });
+
+    return data.matrix;
+  }
+}
---
+++ src/utils/Radar/types.ts
@@ -0,0 +1,91 @@
+export type Address = {
+  latitude: number,
+  longitude: number,
+  geometry: { type: string, coordinates: number[] },
+  country: string,
+  countryCode: string,
+  countryFlag: string,
+  county: string,
+  distance: number,
+  borough: string,
+  city: string,
+  number: string,
+  neighborhood: string,
+  postalCode: string,
+  stateCode: string,
+  state: string,
+  street: string,
+  layer: string,
+  formattedAddress: string,
+  placeLabel: string,
+};
+
+export type AutocompleteParams = {
+  query: string,
+  near?: string,
+  country?: string,
+  limit?: number,
+};
+
+export type AutocompleteResponse = {
+  meta: { code: number },
+  addresses: Address[],
+};
+
+type ValueText = {
+  value: number,
+  text: string,
+};
+
+type Location = {
+  latitude: number,
+  longitude: number,
+};
+
+export type DirectionsParams = {
+  locations: string,
+  /** Defaults to metric */
+  units?: "metric" | "imperial",
+};
+
+export type DirectionsResponse = {
+  meta: { code: number },
+  routes: {
+    distance: ValueText,
+    duration: ValueText,
+    legs: {
+      stateLocation: Location,
+      endLocation: Location,
+      distance: ValueText,
+      duration: ValueText,
+      geometry: { polyline: string },
+    }[],
+  }[],
+};
+
+export type GeocodeParams = {
+  query: string,
+  country?: string,
+};
+
+export type GeocodeResponse = {
+  meta: { code: number },
+  addresses: Address[]
+};
+
+export type MatrixParams = {
+  origins: string,
+  destinations: string,
+  /** Defaults to metric */
+  units?: "metric" | "imperial",
+};
+
+export type MatrixResponse = {
+  meta: { code: number },
+  matrix: {
+    distance: ValueText,
+    duration: ValueText,
+    originIndex: number,
+    destinationIndex: number,
+  }[][],
+};
