96e9da3e1e250157f703ff2eb30f65cb89317d7d
stephen-j-oleary <s.james.oleary@gmail.com>
2024-01-25 00:21:09 -0700

    Use validation middleware

--- src/pages/api/autocomplete.ts
+++ src/pages/api/autocomplete.ts
@@ -1,9 +1,10 @@
 import { filter, isEmpty } from "lodash";
 import cache from "memory-cache";
-import { InferType, number, object, string, ValidationError } from "yup";
+import { InferType, number, object, string } from "yup";
 
 import nextConnect from "@/nextConnect";
 import authorization from "@/nextConnect/middleware/authorization";
+import validation from "@/nextConnect/middleware/validation";
 import { Coordinates } from "@/types/coordinates";
 import { RequestError } from "@/utils/ApiErrors";
 import autocompleteApiClient from "@/utils/autocompleteClient";
@@ -75,19 +76,13 @@ export async function handleGetAutocomplete(params: ApiGetAutocompleteQuery) {
 
 handler.get(
   authorization({ isSubscriber: true }),
+  validation({ query: ApiGetAutocompleteQuerySchema }),
   async (req, res) => {
-    const query = await ApiGetAutocompleteQuerySchema
-      .validate(req.query, { stripUnknown: true })
-      .catch(err => {
-        if (err instanceof ValidationError) throw new RequestError(`Invalid param: ${err.path}`);
-        throw new RequestError("Invalid request");
-      });
-
     const { url } = req;
     const cached = cache.get(url);
     if (cached) return res.status(200).json(cached);
 
-    const data = await handleGetAutocomplete(query);
+    const data = await handleGetAutocomplete(req.query);
     cache.put(url, data, CACHE_TIME);
 
     return res.status(200).json(data);
