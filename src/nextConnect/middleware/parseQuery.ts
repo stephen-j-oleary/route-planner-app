e43e54c49d0c729cac90033a792b9d32f1a1dc1f
stephen-j-oleary <s.james.oleary@gmail.com>
2024-01-24 23:29:53 -0700

    Add number parsing to query string

--- src/nextConnect/middleware/parseQuery.ts
+++ src/nextConnect/middleware/parseQuery.ts
@@ -1,12 +1,12 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { NextHandler } from "next-connect";
-import queryString from "query-string";
+import queryString, { ParsedQuery } from "query-string";
 
 
-export default function parseQuery(req: Omit<NextApiRequest, "query"> & { query: queryString.ParsedQuery<string | boolean> }, _res: NextApiResponse, next: NextHandler) {
+export default function parseQuery(req: Omit<NextApiRequest, "query"> & { query: ParsedQuery<string | number | boolean | undefined> }, _res: NextApiResponse, next: NextHandler) {
   if (req.query) {
     const qs = queryString.stringify(req.query);
-    req.query = queryString.parse(qs, { parseBooleans: true, arrayFormat: "bracket" });
+    req.query = queryString.parse(qs, { parseBooleans: true, parseNumbers: true, arrayFormat: "bracket" });
   }
   next();
 }
