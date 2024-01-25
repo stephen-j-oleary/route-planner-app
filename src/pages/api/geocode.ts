915c2d7bd5a85b9f14aaf63d911bde7426cafa4f
stephen-j-oleary <s.james.oleary@gmail.com>
2024-01-25 00:24:20 -0700

    Add geocode api endpoint

---
+++ src/pages/api/geocode.ts
@@ -0,0 +1,87 @@
+import { filter, isEmpty } from "lodash";
+import cache from "memory-cache";
+import { InferType, object, string } from "yup";
+
+import nextConnect from "@/nextConnect";
+import authorization from "@/nextConnect/middleware/authorization";
+import validation from "@/nextConnect/middleware/validation";
+import { Coordinates } from "@/types/coordinates";
+import radarClient from "@/utils/Radar";
+
+const CACHE_TIME = 5 * 60 * 1000; // 5 mins
+
+
+const handler = nextConnect()
+
+const ApiGetGeocodeQuerySchema = object({
+  q: string().required(),
+  country: string().optional(),
+})
+export type ApiGetGeocodeQuery = InferType<typeof ApiGetGeocodeQuerySchema>
+export type ApiGetGeocodeResponse = {
+  results: {
+    type: string,
+    coordinates: Coordinates,
+    distance: number,
+    fullText: string,
+    mainText: string,
+    secondaryText?: string,
+    number: string,
+    street: string,
+    neighborhood: string,
+    city: string,
+    county: string,
+    postalCode: string,
+    state: string,
+    stateCode: string,
+    country: string,
+    countryCode: string,
+  }[]
+}
+
+export async function handleGetGeocode(params: ApiGetGeocodeQuery) {
+  const res = await radarClient.geocode({
+    query: params.q,
+    country: params.country,
+  });
+
+  const data: ApiGetGeocodeResponse = {
+    results: res.addresses.map(addr => ({
+      type: addr.geometry.type,
+      coordinates: { lat: addr.latitude, lng: addr.longitude },
+      distance: addr.distance,
+      mainText: addr.placeLabel || filter([addr.number, addr.street], v => !isEmpty(v)).join(" "),
+      secondaryText: addr.placeLabel ? addr.formattedAddress : filter([addr.city, addr.stateCode, addr.countryCode], v => !isEmpty(v)).join(", "),
+      fullText: addr.formattedAddress,
+      number: addr.number,
+      street: addr.street,
+      neighborhood: addr.neighborhood,
+      city: addr.city,
+      county: addr.county,
+      postalCode: addr.postalCode,
+      state: addr.state,
+      stateCode: addr.stateCode,
+      country: addr.country,
+      countryCode: addr.countryCode,
+    })),
+  };
+
+  return data;
+}
+
+handler.get(
+  authorization({ isSubscriber: true }),
+  validation({ query: ApiGetGeocodeQuerySchema }),
+  async (req, res) => {
+    const { url } = req;
+    const cached = cache.get(url);
+    if (cached) return res.status(200).json(cached);
+
+    const data = await handleGetGeocode(req.query);
+    cache.put(url, data, CACHE_TIME);
+
+    return res.status(200).json(data);
+  }
+)
+
+export default handler
