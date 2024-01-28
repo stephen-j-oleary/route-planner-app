15e9442ff5356c25ba1c28f7bed3fcc36d51d6a1
stephen-j-oleary <s.james.oleary@gmail.com>
2024-01-28 13:19:34 -0700

    Fix type errors and account linking logic

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -7,11 +7,20 @@ import Account from "@/models/Account";
 import Session from "@/models/Session";
 import User from "@/models/User";
 import VerificationToken from "@/models/VerificationToken";
+import { handleGetAccounts, handlePostAccount } from "@/pages/api/accounts";
 import { handleGetUserById } from "@/pages/api/users/[id]";
 import { NextRequest, NextResponse } from "@/types/next";
 import MongooseAdapter from "@/utils/auth/MongooseAdapter";
 import PasswordProvider from "@/utils/auth/PasswordProvider";
 import connectMongoose from "@/utils/connectMongoose";
+
+const NEXTAUTH_SECRET = process.env.NEXTAUTH_SECRET;
+const GOOGLE_CLIENT_ID = process.env.NEXTAUTH_GOOGLE_ID;
+const GOOGLE_CLIENT_SECRET = process.env.NEXTAUTH_GOOGLE_SECRET;
+
+if (!NEXTAUTH_SECRET) throw new Error("Missing NextAuth secret");
+if (!GOOGLE_CLIENT_ID) throw new Error("Missing Google client id");
+if (!GOOGLE_CLIENT_SECRET) throw new Error("Missing Google client secret");
 
 
 const dbConnect = connectMongoose();
@@ -30,8 +39,8 @@ export const getNextAuthOptions = (req: NextRequest, res: NextResponse) => {
     },
     providers: [
       GoogleProvider({
-        clientId: process.env.NEXTAUTH_GOOGLE_ID,
-        clientSecret: process.env.NEXTAUTH_GOOGLE_SECRET,
+        clientId: GOOGLE_CLIENT_ID,
+        clientSecret: GOOGLE_CLIENT_SECRET,
       }),
       PasswordProvider(dbConnect, models),
     ],
@@ -41,50 +50,42 @@ export const getNextAuthOptions = (req: NextRequest, res: NextResponse) => {
       maxAge: 60 * 60 * 24 * 30, // 30 days
     },
     callbacks: {
+      // Additional logic to handle creating multiple signIn methods per user
       async signIn({ account }) {
+        if (!account) return true; // Continue to the default signIn handling
+
+        // Check the current session for a logged in user
         const currentSession = await getServerSession(req, res, extendedOptions);
         const currentUserId = currentSession?.user?.id;
+        if (!currentUserId) return true; // User is not signed in; Continue to default signIn handling
 
-        // If there is a user already signed in,
-        // and there is an account that is being signed in with
-        if (account && currentUserId) {
-          // Do the account linking
+        // Attempt to link a new account
+        // Check if user already has an account with the provider
+        const existingAccounts = await handleGetAccounts({
+          userId: currentUserId,
+          provider: account.provider,
+        });
+        if (existingAccounts.length) throw new Error("OAuthAccountInUse");
 
-          // Only link accounts that have not yet been linked
-          const existingAccount = await Account.findOne({
-            provider: account.provider,
-            providerAccountId: account.providerAccountId
-          }).lean().exec();
-          if (existingAccount) throw new Error("OAuthAccountInUse");
+        // Link the new account
+        await handlePostAccount({
+          type: "oauth",
+          provider: account.provider,
+          providerAccountId: account.providerAccountId,
+          userId: currentUserId,
+        });
 
-          // Find the account that is currently signed in
-          const currentAccount = await Account.findOne({
-            userId: currentUserId,
-          }).lean().exec();
-
-          // Link the new account
-          await Account.create({
-            type: "oauth",
-            provider: account.provider,
-            providerAccountId: account.providerAccountId,
-            userId: currentUserId,
-          });
-
-          // Remove the signed in account after the new account is created
-          await Account.findByIdAndDelete(currentAccount._id);
-        }
-
-        return true;
+        return true; // Continue to default signIn handling
       },
       async session({ session, token }) {
-        if (session?.user) session.user.id = token.userId;
+        if (session?.user && token.userId) session.user.id = token.userId;
         if (token?.userId) {
           const user = await handleGetUserById(token.userId);
-          token.email = user.email;
-          session.user.name = user.name;
-          session.user.email = user.email;
-          session.user.image = user.image;
-          session.user.customerId = user.customerId;
+          token.email = user?.email;
+          session.user.name = user?.name;
+          session.user.email = user?.email;
+          session.user.image = user?.image;
+          session.user.customerId = user?.customerId;
         }
 
         return session;
@@ -94,7 +95,7 @@ export const getNextAuthOptions = (req: NextRequest, res: NextResponse) => {
         return token;
       },
     },
-    secret: process.env.NEXTAUTH_SECRET,
+    secret: NEXTAUTH_SECRET,
   };
 
   return extendedOptions;
