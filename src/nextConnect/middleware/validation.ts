51fcb64160f39f4bd3d157be435cf0a5f833e737
stephen-j-oleary <s.james.oleary@gmail.com>
2024-01-28 19:16:08 -0700

    Add req and res to the context when validating

--- src/nextConnect/middleware/validation.ts
+++ src/nextConnect/middleware/validation.ts
@@ -10,7 +10,7 @@ export type { InferType as ValidatedType } from "yup";
 export default function validation(schema: ISchema<unknown>) {
   return async function(req: NextApiRequest, res: NextApiResponse, next: NextHandler) {
     req.locals.validated = await schema
-      .validate(req, { stripUnknown: true })
+      .validate(req, { stripUnknown: true, context: { req, res } })
       .catch(err => {
         if (err instanceof ValidationError) throw new RequestError(`Invalid param: ${err.path}`);
         throw new RequestError("Invalid request");
