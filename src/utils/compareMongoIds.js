0c9358201c5bc97edb265d4594da46e93fc59689
stephen-j-oleary <s.james.oleary@gmail.com>
2024-02-01 15:28:59 -0700

    Migrate to typescript

--- src/utils/compareMongoIds.js
+++
@@ -1,5 +0,0 @@
-export default function compareMongoIds(...args) {
-  const argStrings = args.map(item => (item?.toString?.() || item));
-
-  return argStrings.every((v, _i, arr) => v === arr[0]);
-}
