975e58e2c8aee0084b842cead93952fdb29230d5
stephen-j-oleary <s.james.oleary@gmail.com>
2024-02-01 17:16:15 -0700

    Handle auth error for user query

--- src/reactQuery/useUsers.ts
+++ src/reactQuery/useUsers.ts
@@ -1,3 +1,4 @@
+import { AxiosError } from "axios";
 import { useMutation, useQuery, useQueryClient } from "react-query";
 
 import { deleteUser, getUser, GetUserReturn, updateUser } from "@/services/users";
@@ -5,19 +6,31 @@ import { deleteUser, getUser, GetUserReturn, updateUser } from "@/services/users
 const BASE_KEY = "users";
 
 
-export type UseGetUserOptions<TData, TSelected> = {
+export type UseGetUserOptions<TSelected> = {
   enabled?: boolean,
-  select?: (data: TData) => TSelected,
+  select?: (data: Awaited<GetUserReturn>) => TSelected,
   retry?: number | boolean,
   retryOnMount?: boolean,
   refetchOnMount?: boolean | "always",
   refetchOnWindowFocus?: boolean | "always",
 };
 
-export function useGetUser<TData = Awaited<GetUserReturn>, TSelected = TData>(options: UseGetUserOptions<TData, TSelected> = {}) {
+export function useGetUser<TSelected = Awaited<GetUserReturn>>(options: UseGetUserOptions<TSelected> = {}) {
   return useQuery({
     queryKey: [BASE_KEY],
-    queryFn: () => getUser() as TData,
+    queryFn: async () => {
+      try {
+        const data = await getUser();
+        return data;
+      }
+      catch (err) {
+        if (err instanceof AxiosError) {
+          if (err.response?.status === 401) return null;
+          throw err.response?.data;
+        }
+        throw err;
+      }
+    },
     ...options,
   });
 }
