2646c87c44bf18e8dbe7e46295aacf283bfef619
stephen-j-oleary <s.james.oleary@gmail.com>
2024-02-04 21:09:33 -0700

    Add default values for verification token documents

--- src/models/VerificationToken.ts
+++ src/models/VerificationToken.ts
@@ -2,6 +2,9 @@ import mongoose from "mongoose";
 import { VerificationToken as AdapterVerificationToken } from "next-auth/adapters";
 
 
+const TOKEN_DIGITS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const TOKEN_LENGTH = 5;
+
 export interface IVerificationToken extends AdapterVerificationToken {
   _id: string | mongoose.Types.ObjectId;
   expires: Date;
@@ -15,10 +18,15 @@ const verificationTokenSchema = new mongoose.Schema<IVerificationToken, IVerific
   expires: {
     type: Date,
     trim: true,
+    default: () => Date.now() + 1000 * 60 * 60 * 3, // 3 hours from now
   },
   token: {
     type: String,
     trim: true,
+    default: () => Array(TOKEN_LENGTH)
+      .fill(0)
+      .map(() => TOKEN_DIGITS[Math.floor(Math.random() * TOKEN_DIGITS.length)])
+      .join(""),
   },
   identifier: {
     type: String,
