b5e24b8e4f1c5640f732d3e6cc91e756bf37992c
stephen-j-oleary <s.james.oleary@gmail.com>
2024-02-06 20:57:29 -0700

    Update main error handler

--- src/nextConnect/middleware/mongoose.ts
+++ src/nextConnect/middleware/mongoose.ts
@@ -1,4 +1,4 @@
-import mongoose, { MongooseError } from "mongoose";
+import { MongooseError } from "mongoose";
 import { NextApiRequest, NextApiResponse } from "next";
 import { NextHandler } from "next-connect";
 
@@ -14,27 +14,4 @@ export default async function mongooseMiddleware(_req: NextApiRequest, res: Next
     if (err instanceof MongooseError) res.status(500).json({ message: err.message });
     return res.status(500).json(err);
   }
-}
-
-export function handleMongooseError(err: unknown) {
-  if (err instanceof mongoose.Error.ValidationError) {
-    const errors = Object.values(err.errors).map(({ path, message }) => ([path, message]));
-
-    return {
-      status: 400,
-      message: "Invalid request",
-      field: Object.fromEntries(errors),
-    };
-  }
-
-  if (err instanceof mongoose.Error) {
-    if (err.message.startsWith("E11000")) {
-      return {
-        status: 409,
-        message: "Duplicate field",
-      };
-    }
-  }
-
-  return err;
 }
