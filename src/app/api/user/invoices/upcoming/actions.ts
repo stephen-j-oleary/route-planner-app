cff3b8bf15792a729c5971a5ceedbae65d08f558
stephen-j-oleary <s.james.oleary@gmail.com>
2024-09-28 17:31:49 -0600

    Fix post upcoming invoice action

--- src/app/api/user/invoices/upcoming/actions.ts
+++ src/app/api/user/invoices/upcoming/actions.ts
@@ -21,8 +21,8 @@ export async function postUserUpcomingInvoice({ subscription_cancel_at, subscrip
 
   const invoice = await stripeClientNext.invoices.retrieveUpcoming({
     customer: customerId,
-    subscription_cancel_at: subscription_cancel_at && subscription_cancel_at.valueOf() / 1000,
-    subscription_proration_date: subscription_proration_date && subscription_proration_date.valueOf() / 1000,
+    subscription_cancel_at: subscription_cancel_at && Math.round(subscription_cancel_at.valueOf() / 1000), // Round to integer of seconds; The value passed to Stripe must be a whole integer
+    subscription_proration_date: subscription_proration_date && Math.round(subscription_proration_date.valueOf() / 1000), // Round to integer of seconds; The value passed to Stripe must be a whole integer
     ...params,
   });
   if (!invoice) throw new ApiError(500, "Resource not created");
