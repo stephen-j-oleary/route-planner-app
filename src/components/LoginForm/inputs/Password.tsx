fe892c1dc39a131eddae6c3090c459af5e2ea3bd
stephen-j-oleary <s.james.oleary@gmail.com>
2024-12-17 15:28:13 -0700

    Update password strength ui

--- src/components/LoginForm/inputs/Password.tsx
+++ src/components/LoginForm/inputs/Password.tsx
@@ -1,17 +1,14 @@
 import "client-only";
 
-import { bindHover, bindMenu, usePopupState } from "material-ui-popup-state/hooks";
-import HoverMenu from "material-ui-popup-state/HoverMenu";
 import React from "react";
 
-import { InfoRounded, Visibility, VisibilityOff } from "@mui/icons-material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
 import { Box, IconButton, InputAdornment, Stack, TextField, TextFieldProps, Tooltip, Typography } from "@mui/material";
 
 import SplitLinearProgress from "@/components/ui/SplitLinearProgress";
 import passwordStrength from "@/utils/passwordStrength";
 
 
-const STRENGTH_NAMES = ["none", "weak", "ok", "strong", "very strong"];
 const STRENGTH_COLORS = ["error", "error", "warning", "success", "success"] as const;
 
 
@@ -31,11 +28,6 @@ export default function LoginFormPasswordInput({
   label = isNew ? "Create a Password" : "Password",
   ...props
 }: LoginFormPasswordInputProps) {
-  const passwordStrengthPopup = usePopupState({
-    popupId: "passsword-strength",
-    variant: "dialog",
-  });
-
   const [showPassword, setShowPassword] = React.useState(false);
 
   const strength = React.useMemo(
@@ -80,61 +72,22 @@ export default function LoginFormPasswordInput({
 
   return isNew
     ? (
-      <Stack>
+      <Stack spacing={1}>
         {field}
 
-        <Stack
-          direction="row"
-          justifyContent="space-between"
-          alignItems="center"
-          spacing={1}
-        >
-          <Box flexGrow={1}>
-            <SplitLinearProgress
-              value={strength.score * 25}
-              color={STRENGTH_COLORS[strength.score || 0]}
-              segmentCount={4}
-            />
+        <Box>
+          <SplitLinearProgress
+            value={strength.score * 25}
+            color={STRENGTH_COLORS[strength.score || 0]}
+            segmentCount={4}
+          />
 
-            <Typography
-              variant="caption"
-            >
-              Password strength: {STRENGTH_NAMES[strength.score]}
-            </Typography>
-          </Box>
-
-          {
-            strength.feedback?.warning && (
-              <>
-                <IconButton
-                  size="small"
-                  onClick={() => passwordStrengthPopup.toggle()}
-                  {...bindHover(passwordStrengthPopup)}
-                >
-                  <InfoRounded />
-                </IconButton>
-
-                <HoverMenu
-                  {...bindMenu(passwordStrengthPopup)}
-                  anchorOrigin={{
-                    vertical: "bottom",
-                    horizontal: "right",
-                  }}
-                  transformOrigin={{
-                    vertical: "top",
-                    horizontal: "right",
-                  }}
-                >
-                  <Typography
-                    variant="caption"
-                  >
-                    {strength.feedback.warning}
-                  </Typography>
-                </HoverMenu>
-              </>
-            )
-          }
-        </Stack>
+          <Typography
+            variant="caption"
+          >
+            &nbsp;{strength.feedback?.warning || ""}
+          </Typography>
+        </Box>
       </Stack>
     )
     : field;
