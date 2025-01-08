e4ab75e9c2e255293d171538f9cc6e1ece11d275
stephen-j-oleary <s.james.oleary@gmail.com>
2025-01-07 21:44:48 -0700

    Add "Why Us" section and simplify layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -81,138 +81,175 @@ export default function HomePage() {
         </Stack>
       </Box>
 
-      <Container maxWidth="lg" sx={{ py: 5 }}>
-        <Typography variant="h2" pb={4}>
-          How does Loop Mapping work?
-        </Typography>
-
-        <Box
-          display="grid"
-          gridTemplateColumns={{
-            sm: "1fr",
-            md: "auto 1fr 1fr",
-          }}
-          columnGap={2}
-        >
-          <Typography variant="h3" color="text.secondary" py={2} pr={1} gridColumn={1}>
-            Step 1
-          </Typography>
-
-          <Box gridColumn={{ sm: 1, md: 2 }} py={2}>
-            <Typography variant="h4" pb={1}>
-              Enter your stops
+      <Container maxWidth="lg" sx={{ py: 5, "& h2": { lineHeight: 1.8 } }}>
+        <Stack spacing={4}>
+          <div>
+            <Typography variant="h2">
+              Why Loop Mapping?
             </Typography>
 
-            <Typography variant="body1">
-              Begin by entering all the stops along your route in any order
+            <Stack spacing={2} py={2}>
+              <div>
+                <Typography variant="h3">Smart Efficiency</Typography>
+
+                <Typography variant="body1">
+                  Automatically find the best route to minimize time and fuel costs.
+                </Typography>
+              </div>
+
+              <div>
+                <Typography variant="h3">Intuitive Design</Typography>
+
+                <Typography variant="body1">
+                  Built for simplicity—just add stops and get optimized results.
+                </Typography>
+              </div>
+
+              <div>
+                <Typography variant="h3">Versatile Use</Typography>
+
+                <Typography variant="body1">
+                  Perfect for personal travel, small business deliveries, or field operations.
+                </Typography>
+              </div>
+            </Stack>
+          </div>
+
+          <div>
+            <Typography variant="h2">
+              How does Loop Mapping work?
             </Typography>
-          </Box>
 
-          <Typography variant="h3" color="text.secondary" py={2} gridColumn={1}>
-            Step 2
-          </Typography>
-
-          <Box gridColumn={{ sm: 1, md: 2 }} py={2} sx={{ borderTopColor: "divider", borderTopWidth: "1px", borderTopStyle: "solid" }}>
-            <Typography variant="h4" pb={1}>
-              Select your origin and destination
-            </Typography>
-
-            <Typography variant="body1">
-              Select where you will be starting and ending your trip
-            </Typography>
-          </Box>
-
-          <Paper
-            elevation={4}
-            sx={{
-              background: "transparent",
-              position: "relative",
-              minHeight: "30dvh",
-              gridColumn: { sm: 1, md: 3 },
-              gridRow: { sm: "auto", md: "1 / span 2" },
-              overflow: "hidden",
-            }}
-          >
-            <Image
-              src="/screenshots/mockup-enterstops.png"
-              alt="Enter stops mockup"
-              fill
-              sizes="100%"
-              style={{
-                objectFit: "cover",
-                objectPosition: "top left",
+            <Box
+              display="grid"
+              gridTemplateColumns={{
+                sm: "1fr",
+                md: "auto 1fr 1fr",
               }}
-            />
-          </Paper>
-
-          <Box gridColumn={{ sm: 1, md: "2 / -1" }} height="1px" my={2} sx={{ backgroundColor: "divider" }} />
-
-          <Typography variant="h3" color="text.secondary" py={2} gridColumn={1}>
-            Step 3
-          </Typography>
-
-          <Box gridColumn={{ sm: 1, md: 3 }} py={2}>
-            <Typography variant="h4" pb={1}>
-              Add stop time
-            </Typography>
-
-            <Typography variant="body1">
-              Optionally, include a stop time to add for each location on your loop
-            </Typography>
-          </Box>
-
-          <Typography variant="h3" color="text.secondary" py={2} gridColumn={1}>
-            Step 4
-          </Typography>
-
-          <Box gridColumn={{ sm: 1, md: 3 }} py={2} sx={{ borderTopColor: "divider", borderTopWidth: "1px", borderTopStyle: "solid" }}>
-            <Typography variant="h4" pb={1}>
-              Calculate the route
-            </Typography>
-
-            <Typography variant="body1">
-              Hit enter and view your optimized route
-            </Typography>
-          </Box>
-
-          <Paper
-            elevation={4}
-            sx={{
-              background: "transparent",
-              position: "relative",
-              minHeight: "30dvh",
-              gridColumn: { sm: 1, md: 2 },
-              gridRow: { sm: "auto", md: "4 / span 2" },
-              overflow: "hidden",
-            }}
-          >
-            <Image
-              src="/screenshots/mockup-calculated.png"
-              alt="Calculated route mockup"
-              fill
-              sizes="100%"
-              style={{
-                objectFit: "cover",
-                objectPosition: "top left",
-              }}
-            />
-          </Paper>
-
-          <Box gridColumn={{ sm: 1, md: "2 / -1" }} height="1px" my={2} sx={{ backgroundColor: "divider" }} />
-
-          <Stack alignItems="center" gridColumn="1 / -1">
-            <Button
-              variant="contained"
-              size="large"
-              color="primary"
-              component={Link}
-              href={pages.routes.new}
-              sx={{ my: 2 }}
+              columnGap={2}
+              pt={2}
             >
-              Get started for free
-            </Button>
-          </Stack>
-        </Box>
+              <Typography variant="h3" color="text.secondary" py={2} pr={1} gridColumn={1}>
+                Step 1
+              </Typography>
+
+              <Box gridColumn={{ sm: 1, md: 2 }} py={2}>
+                <Typography variant="h4" pb={1}>
+                  Enter your stops
+                </Typography>
+
+                <Typography variant="body1">
+                  Begin by entering all the stops along your route in any order
+                </Typography>
+              </Box>
+
+              <Typography variant="h3" color="text.secondary" py={2} gridColumn={1}>
+                Step 2
+              </Typography>
+
+              <Box gridColumn={{ sm: 1, md: 2 }} py={2} sx={{ borderTopColor: "divider", borderTopWidth: "1px", borderTopStyle: "solid" }}>
+                <Typography variant="h4" pb={1}>
+                  Select your origin and destination
+                </Typography>
+
+                <Typography variant="body1">
+                  Select where you will be starting and ending your trip
+                </Typography>
+              </Box>
+
+              <Paper
+                elevation={4}
+                sx={{
+                  background: "transparent",
+                  position: "relative",
+                  minHeight: "30dvh",
+                  gridColumn: { sm: 1, md: 3 },
+                  gridRow: { sm: "auto", md: "1 / span 2" },
+                  overflow: "hidden",
+                }}
+              >
+                <Image
+                  src="/screenshots/mockup-enterstops.png"
+                  alt="Enter stops mockup"
+                  fill
+                  sizes="100%"
+                  style={{
+                    objectFit: "cover",
+                    objectPosition: "top left",
+                  }}
+                />
+              </Paper>
+
+              <Box gridColumn={{ sm: 1, md: "2 / -1" }} height="1px" my={2} sx={{ backgroundColor: "divider" }} />
+
+              <Typography variant="h3" color="text.secondary" py={2} gridColumn={1}>
+                Step 3
+              </Typography>
+
+              <Box gridColumn={{ sm: 1, md: 3 }} py={2}>
+                <Typography variant="h4" pb={1}>
+                  Add stop time
+                </Typography>
+
+                <Typography variant="body1">
+                  Optionally, include a stop time to add for each location on your loop
+                </Typography>
+              </Box>
+
+              <Typography variant="h3" color="text.secondary" py={2} gridColumn={1}>
+                Step 4
+              </Typography>
+
+              <Box gridColumn={{ sm: 1, md: 3 }} py={2} sx={{ borderTopColor: "divider", borderTopWidth: "1px", borderTopStyle: "solid" }}>
+                <Typography variant="h4" pb={1}>
+                  Calculate the route
+                </Typography>
+
+                <Typography variant="body1">
+                  Hit enter and view your optimized route
+                </Typography>
+              </Box>
+
+              <Paper
+                elevation={4}
+                sx={{
+                  background: "transparent",
+                  position: "relative",
+                  minHeight: "30dvh",
+                  gridColumn: { sm: 1, md: 2 },
+                  gridRow: { sm: "auto", md: "4 / span 2" },
+                  overflow: "hidden",
+                }}
+              >
+                <Image
+                  src="/screenshots/mockup-calculated.png"
+                  alt="Calculated route mockup"
+                  fill
+                  sizes="100%"
+                  style={{
+                    objectFit: "cover",
+                    objectPosition: "top left",
+                  }}
+                />
+              </Paper>
+
+              <Box gridColumn={{ sm: 1, md: "2 / -1" }} height="1px" my={2} sx={{ backgroundColor: "divider" }} />
+
+              <Stack alignItems="center" gridColumn="1 / -1">
+                <Button
+                  variant="contained"
+                  size="large"
+                  color="primary"
+                  component={Link}
+                  href={pages.routes.new}
+                  sx={{ my: 2 }}
+                >
+                  Get started for free
+                </Button>
+              </Stack>
+            </Box>
+          </div>
+        </Stack>
       </Container>
     </>
   );
