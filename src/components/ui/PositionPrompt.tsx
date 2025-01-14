c2449deb3117d534143f2b006b6ac955801f762f
stephen-j-oleary <s.james.oleary@gmail.com>
2025-01-13 22:51:49 -0700

    Use toast for position prompt

--- src/components/ui/PositionPrompt.tsx
+++ src/components/ui/PositionPrompt.tsx
@@ -1,6 +1,9 @@
 import NextLink from "next/link";
+import { useEffect } from "react";
+import { toast } from "react-toastify";
 
-import { Alert, Button } from "@mui/material";
+import { CloseRounded } from "@mui/icons-material";
+import { Alert, Button, IconButton } from "@mui/material";
 
 import useGeolocation from "@/hooks/useGeolocation";
 import pages from "pages";
@@ -9,18 +12,37 @@ import pages from "pages";
 export default function PositionPrompt() {
   const geolocation = useGeolocation();
 
-  if (["loading", "granted"].includes(geolocation.state)) return null;
+  useEffect(
+    () => {
+      if (["loading", "granted"].includes(geolocation.state)) return;
 
-  return (
-    <Alert
-      severity="info"
-      action={
-        geolocation.state === "prompt"
-          ? <Button onClick={() => geolocation.request()}>Allow</Button>
-          : <Button component={NextLink} href={pages.enableLocation} target="_blank">Allow</Button>
-      }
-    >
-      Allow location access to get more accurate address and place suggestions
-    </Alert>
+      const id = toast(
+        ({ closeToast }) => (
+          <Alert
+            severity="info"
+            action={
+              <>
+                {
+                  geolocation.state === "prompt"
+                    ? <Button onClick={() => geolocation.request()}>Allow</Button>
+                    : <Button component={NextLink} href={pages.enableLocation} target="_blank">Allow</Button>
+                }
+
+                <IconButton onClick={() => closeToast()}>
+                  <CloseRounded />
+                </IconButton>
+              </>
+            }
+          >
+            Allow location access to get more accurate address and place suggestions
+          </Alert>
+        ),
+      );
+
+      return () => toast.dismiss(id);
+    },
+    [geolocation]
   );
+
+  return null;
 }
