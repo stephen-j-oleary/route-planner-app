d7771bd02d4639656de28202f7f04aed96059fa0
stephen-j-oleary <s.james.oleary@gmail.com>
2025-01-16 15:09:28 -0700

    Add userEmail query to getAccounts

--- src/app/api/accounts/actions.ts
+++ src/app/api/accounts/actions.ts
@@ -1,12 +1,20 @@
 "use server";
 
 import { ApiGetAccountsQuery } from "@/app/api/accounts/schemas";
+import { getUsers } from "@/app/api/users/actions";
 import Account, { accountPublicFields } from "@/models/Account";
 import connectMongoose from "@/utils/connectMongoose";
 
 
-export async function getAccounts(query: ApiGetAccountsQuery) {
+export async function getAccounts({ userEmail, ...query }: ApiGetAccountsQuery) {
   await connectMongoose();
+
+  if (userEmail) {
+    const [user] = await getUsers({ email: userEmail });
+    if (!user) return [];
+
+    query.userId = user._id.toString();
+  }
 
   return await Account.find(query, accountPublicFields).lean().exec();
 }
--- src/app/api/accounts/schemas.ts
+++ src/app/api/accounts/schemas.ts
@@ -8,7 +8,16 @@ export const ApiGetAccountsQuerySchema = object()
   .shape({
     userId: string()
       .typeError("Invalid user id")
-      .required("Missing user id"),
+      .when("userEmail", {
+        is: (val: unknown) => !val,
+        then: schema => schema.required("Missing user id or user email"),
+      }),
+    userEmail: string()
+      .typeError("Invalid user email")
+      .when("userId", {
+        is: (val: unknown) => !val,
+        then: schema => schema.required("Missing user id or user email"),
+      }),
     provider: string()
       .typeError("Invalid provider")
       .optional(),
