9301b89acf6217095d17a0a12b24f4ba7b9b2799
stephen-j-oleary <s.james.oleary@gmail.com>
2025-01-15 19:29:48 -0700

    Remove upcoming invoice It was throwing an error when the customer didn't have any subscriptions

--- src/app/account/@invoices/page.tsx
+++ src/app/account/@invoices/page.tsx
@@ -2,7 +2,6 @@ import { cookies } from "next/headers";
 
 import { getPrices } from "@/app/api/prices/actions";
 import { getUserInvoices } from "@/app/api/user/invoices/actions";
-import { getUserUpcomingInvoice } from "@/app/api/user/invoices/upcoming/actions";
 import InvoicesList from "@/components/Invoices/List";
 import { StripePriceActiveExpandedProduct } from "@/models/Price";
 import { auth } from "@/utils/auth";
@@ -12,16 +11,12 @@ import pojo from "@/utils/pojo";
 export default async function InvoicesPage() {
   const { customerId } = await auth(cookies());
 
-  const upcoming = customerId ? pojo(await getUserUpcomingInvoice({ customer: customerId })) : null;
   const invoices = customerId ? pojo(await getUserInvoices({ customer: customerId })) : [];
   const prices = customerId ? pojo(await getPrices({ active: true, expand: ["data.product"] }) as StripePriceActiveExpandedProduct[]) : [];
 
   return (
     <InvoicesList
-      invoices={[
-        ...(upcoming ? [upcoming] : []),
-        ...invoices
-      ]}
+      invoices={invoices}
       prices={prices}
       visible={3}
     />
