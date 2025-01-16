49fa128e5bfbb6e88aa468a415b671295f360d78
stephen-j-oleary <s.james.oleary@gmail.com>
2025-01-16 13:54:18 -0700

    Handle skipping undefined params

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,9 +1,10 @@
-export function appendQuery(url: string | URL, params: Record<string, string>) {
+export function appendQuery(url: string | URL, params: Record<string, string | undefined>) {
   const _url = new URL(url, process.env.NEXT_PUBLIC_BASE_URL);
   const query = _url.searchParams;
 
   for (const key in params) {
-    query.set(key, params[key]);
+    const value = params[key];
+    if (value) query.set(key, value);
   }
 
   return `${_url.origin}${_url.pathname}?${query.toString()}`;
