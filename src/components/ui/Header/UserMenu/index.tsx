31297eea1f79a518c0033a78ed6c744cf76b20c2
stephen-j-oleary <s.james.oleary@gmail.com>
2025-01-15 17:57:41 -0700

    Hide user menu on login page

--- src/components/ui/Header/UserMenu/index.tsx
+++ src/components/ui/Header/UserMenu/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { useState } from "react";
 
 import { CloseRounded } from "@mui/icons-material";
@@ -18,8 +19,12 @@ export type UserMenuProps = {
 export default function UserMenu({
   session,
 }: UserMenuProps) {
+  const pathname = usePathname();
+
   const [open, setOpen] = useState(false);
 
+
+  if (pathname === pages.login) return null;
 
   if (!session.userId) {
     return (
