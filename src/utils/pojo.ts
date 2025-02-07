6125a3c249f8643fccc8a9f2f7540a045e6aa12e
stephen-j-oleary <s.james.oleary@gmail.com>
2025-02-07 03:50:02 -0700

    Fix Pojo type

--- src/utils/pojo.ts
+++ src/utils/pojo.ts
@@ -1,10 +1,12 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export type Pojo<TObject extends object> = {
-  [Key in keyof TObject]: string | number extends TObject[Key]
-    ? TObject[Key]
-    : TObject[Key] extends object
+  [Key in keyof TObject]: TObject[Key] extends object
     ? Pojo<TObject[Key]>
-    : never;
+    : TObject[Key] extends (...args: any[]) => any
+    ? never
+    : TObject[Key];
 }
+/* eslint-enable @typescript-eslint/no-explicit-any */
 
 export default function pojo<TObject extends object>(obj: TObject): Pojo<TObject> {
   return JSON.parse(JSON.stringify(obj));
