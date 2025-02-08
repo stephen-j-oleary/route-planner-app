fbfcf617bf71b065e11eaca78634fb711b7bb750
stephen-j-oleary <s.james.oleary@gmail.com>
2025-02-08 15:56:35 -0700

    Update consent manager

--- src/app/CookieConsent.tsx
+++ src/app/CookieConsent.tsx
@@ -4,19 +4,13 @@ export default function CookieConsent() {
   //return (<script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="4b6b1847-891e-4016-a9ff-2aeadd8941dd" type="text/javascript" async></script>);
 
   return (
-    <>
-      <Script
-        type="text/javascript"
-        dangerouslySetInnerHTML={{
-          __html: `
-            var _iub = _iub || [];
-            _iub.csConfiguration = {"siteId":3923456,"cookiePolicyId":17663063,"lang":"en","storage":{"useSiteId":true}};
-          `,
-        }}
-      />
-      <Script type="text/javascript" src="https://cs.iubenda.com/autoblocking/3923456.js" />
-      <Script type="text/javascript" src="//cdn.iubenda.com/cs/gpp/stub.js" />
-      <Script type="text/javascript" src="//cdn.iubenda.com/cs/iubenda_cs.js" charSet="UTF-8" async />
-    </>
+    <Script
+      type="text/javascript"
+      data-cmp-ab="1"
+      src="https://cdn.consentmanager.net/delivery/autoblocking/15f25cc122296.js"
+      data-cmp-host="a.delivery.consentmanager.net"
+      data-cmp-cdn="cdn.consentmanager.net"
+      data-cmp-codesrc="16"
+    />
   );
 }
