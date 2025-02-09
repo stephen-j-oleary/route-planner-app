5f128a4744af90400a97e1961a77b537aed79e0e
stephen-j-oleary <s.james.oleary@gmail.com>
2025-02-08 17:04:21 -0700

    Add google consent mode

--- src/app/CookieConsent.tsx
+++ src/app/CookieConsent.tsx
@@ -2,23 +2,44 @@ import Script from "next/script";
 
 export default function CookieConsent() {
   return (
-    <Script
-      async
-      type="text/javascript"
-      src="https://consent.cookiebot.com/uc.js"
-      id="Cookiebot"
-      data-cbid="4b6b1847-891e-4016-a9ff-2aeadd8941dd"
-    />
-  );
+    <>
+      <Script
+        async
+        type="text/javascript"
+        strategy="afterInteractive"
+        src="https://consent.cookiebot.com/uc.js"
+        id="Cookiebot"
+        data-cbid="4b6b1847-891e-4016-a9ff-2aeadd8941dd"
+      />
 
-  /* return (
-    <Script
-      type="text/javascript"
-      data-cmp-ab="1"
-      src="https://cdn.consentmanager.net/delivery/autoblocking/15f25cc122296.js"
-      data-cmp-host="a.delivery.consentmanager.net"
-      data-cmp-cdn="cdn.consentmanager.net"
-      data-cmp-codesrc="16"
-    />
-  ); */
+      <Script
+        strategy="afterInteractive"
+        data-cookieconsent="ignore"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag() {
+              dataLayer.push(arguments);
+            }
+            gtag(
+              "consent",
+              "default",
+              {
+                ad_personalization: "denied",
+                ad_storage: "denied",
+                ad_user_data: "denied",
+                analytics_storage: "denied",
+                functionality_storage: "denied",
+                personalization_storage: "denied",
+                security_storage: "granted",
+                wait_for_update: 500,
+              }
+            );
+            gtag("set", "ads_data_redaction", true);
+            gtag("set", "url_passthrough", false);
+          `,
+        }}
+      />
+    </>
+  );
 }
