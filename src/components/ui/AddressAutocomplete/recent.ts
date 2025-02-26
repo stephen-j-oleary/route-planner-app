83402b929dca14650ec8d0d99ea819b7dfe7895c
stephen-j-oleary <s.james.oleary@gmail.com>
2025-02-26 14:07:33 -0700

    Add max stops and sorting by most reecent

--- src/components/ui/AddressAutocomplete/recent.ts
+++ src/components/ui/AddressAutocomplete/recent.ts
@@ -1,15 +1,24 @@
 import { useLiveQuery } from "dexie-react-hooks";
 
 import { RecentStops, TStop } from "@/models/Stop";
+import idb from "@/utils/idb";
 
+
+const MAX_RECENT_STOPS = 10;
 
 export async function saveRecentStop(stop: TStop) {
-  await RecentStops.where({ fullText: stop.fullText }).delete();
-  await RecentStops.put(stop);
+  await idb.transaction("rw", "recentStops", async () => {
+    await RecentStops.where({ fullText: stop.fullText }).delete();
+
+    const count = await RecentStops.count();
+    if (count >= MAX_RECENT_STOPS) await RecentStops.orderBy(":id").limit(1).delete();
+
+    await RecentStops.put(stop);
+  })
 }
 
 export function useRecentStops() {
-  const data = useLiveQuery(() => RecentStops.toArray());
+  const data = useLiveQuery(() => RecentStops.orderBy(":id").reverse().toArray());
 
   return { data };
 }
